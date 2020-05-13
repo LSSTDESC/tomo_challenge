b42e83123ee3a3f4d63587062f90a793cce63109
Francois <francois@orioncloud.fr>
2020-05-13 12:11:44 +0200

    Adds better NN class

--- classifiers/neural_network.py
+++ classifiers/neural_network.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as onp
 
 import os.path
 import pickle
@@ -16,6 +16,43 @@ from sklearn.preprocessing import StandardScaler
 
 # Conviniently stores the number of features
 n_features = {'riz':6, 'griz':10}
+
+# Function creating the neural network for a specific number of bins
+def get_classifier(n_bin, n_features):
+    # Let's create a cute little neural network for classification
+    class BinningNN(nn.Module):
+        def apply(self, x):
+            net = nn.leaky_relu(nn.Dense(x,   500, name='fc1'))
+            net = nn.leaky_relu(nn.Dense(net, 500, name='fc2'))
+            return nn.softmax(nn.Dense(net, n_bin))
+    # Initializing neural network weights for this configuration
+    _, initial_params = BinningNN.init_by_shape( rand.PRNGKey(0),
+                                                [((1, n_features), np.float32)])
+    # This instantiates the model, now ready to use
+    return nn.Model(BinningNN, initial_params)
+
+# This defines the optimization step, literally maximizing the S/N
+def get_optimizing_step(metric):
+    if metric == 'SNR':
+        score_fn = metrics.compute_snr_score
+    elif metric == 'FOM':
+        score_fn = metrics.compute_fom_score
+
+    @jax.jit
+    def train_step(optimizer, batch):
+        # This is the loss function
+        def loss_fn(model):
+            # Apply classifier to features
+            w = model(batch['features'])
+            # returns - score, because we want to maximize score
+            return - score_fn(w, batch['labels'])
+        # Compute gradients
+        loss, g = jax.value_and_grad(loss_fn)(optimizer.target)
+        # Perform gradient descent
+        optimizer = optimizer.apply_gradient(g)
+        return optimizer, loss
+
+    return train_step
 
 class NeuralNetwork:
     """ Neural Network Classifier """
@@ -45,31 +82,17 @@ class NeuralNetwork:
 
         self.n_features = n_features[bands]
         self.n_bin = options['bins']
+        self.metric = options['metric']
+        # Build a name for the model based on bands and options
+        self.export_name = f'models/{self.metric}_{bands}_{self.n_bin}.flax'
 
         # Create classifier
-        self.model = self.get_classifier()
+        self.model = get_classifier(self.n_bin, self.n_features)
         # Create scaler
         self.features_scaler = StandardScaler()
 
-    def get_classifier(self):
-        """
-        Function creating the neural network for a specific number of bins
-        """
-        n_bin = self.n_bin
-        n_features = self.n_features
-        # Let's create a cute little neural network for classification
-        class BinningNN(nn.Module):
-            def apply(self, x):
-                net = nn.leaky_relu(nn.Dense(x,   500, name='fc1'))
-                net = nn.leaky_relu(nn.Dense(net, 500, name='fc2'))
-                return nn.softmax(nn.Dense(net, n_bin))
-        # Initializing neural network weights for this configuration
-        _, initial_params = BinningNN.init_by_shape( rand.PRNGKey(0),
-                                                    [((1, n_features), np.float32)])
-        # This instantiates the model, now ready to use
-        return nn.Model(BinningNN, initial_params)
-
-    def train (self, training_data, training_z):
+    def train (self, training_data, training_z,
+              batch_size=5000, niter=1500):
         """Trains the classifier
 
         Parameters:
@@ -82,11 +105,39 @@ class NeuralNetwork:
 
         """
         # create scaler
-        self.features_scaler.fit(training_data)
+        features = self.features_scaler.fit_transform(training_data)
+        labels = training_z
 
-        # For now, I'm just grabbing the trained 2b neural network
-        with open('models/BinningNN_3x2_2b_FoM.flax', 'rb') as file:
+        # If model is already trained, we just load the weights
+        if os.path.exists(self.export_name):
+            with open(self.export_name, 'rb') as file:
             self.model = serialization.from_bytes(self.model, pickle.load(file))
+            return
+
+        # Otherwise we train
+        if self.metric == 'FOM':
+            lr = 0.1
+        if self.metric == 'SNR':
+            lr = 0.001
+        optimizer = optim.Momentum(learning_rate=lr, beta=0.9).create(self.model)
+        # Create training fn for specific metric
+        train_step = get_optimizing_step(self.metric)
+
+        # This function provides random batches of data, TODO: convert to JAX
+        def get_batch():
+            inds = onp.random.choice(len(labels), batch_size)
+            return {'labels': labels[inds], 'features': features[inds]}
+
+        losses = []
+        for i in range(niter):
+            optimizer, loss = train_step(optimizer, get_batch())
+            losses.append(loss)
+            if i%100 == 0:
+                print('iter: %d; Loss : %f'%(i,loss))
+
+        # Export model to disk
+        with open(self.export_name, 'wb') as file:
+            pickle.dump(serialization.to_bytes(optimizer.target), file)
 
     def apply (self, data):
         """Applies training to the data.
