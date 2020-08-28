a6465c1b0f3b6ca9bc062b980a257da268d7da60
Bela Abolfathi <babolfat@uci.edu>
2020-08-28 12:23:16 -0700

    Add updated snc.py

--- the_atlantics/snc.py
+++ the_atlantics/snc.py
@@ -5,20 +5,19 @@ import os
 
 
 assign_params_default = {'p_inbin_thr': 0.5,
-                         'p_outbin_thr': 0.2,
-                         'use_p_inbin': False,
-                         'use_p_outbin': False}
+        'p_outbin_thr': 0.2,
+        'use_p_inbin': False,
+        'use_p_outbin': False}
 
 
 class SnCalc(object):
     edges_large = 100.
 
     def __init__(self, z_arr, nz_list, fsky=0.4, lmax=2000, d_ell=10,
-                 s_gamma=0.28, use_clustering=False):
+            s_gamma=0.28, use_clustering=False):
         """ S/N calculator
-
         Args:
-            z_arr (array_like): array of redshifts at which all the N(z)s are
+                        z_arr (array_like): array of redshifts at which all the N(z)s are
                 sampled. They should be linearly spaced.
             nz_list (list): list of arrays containing the redshift
                 distributions of all the initial groups (each array in the
@@ -44,10 +43,10 @@ class SnCalc(object):
         self.z_arr = z_arr
         self.nz_list = nz_list
         self.n_gals = np.array([simps(nz, x=self.z_arr)
-                                for nz in self.nz_list])
+            for nz in self.nz_list])
         self.pz_list = self.nz_list / self.n_gals[:, None]
         self.z_means = np.array([simps(self.z_arr*nz, x=self.z_arr)
-                                 for nz in self.nz_list]) / self.n_gals
+            for nz in self.nz_list]) / self.n_gals
         self.n_dens = self.n_gals / (4*np.pi*self.fsky)
         self.cls = None
         self.use_clustering = use_clustering
@@ -57,9 +56,8 @@ class SnCalc(object):
 
     def get_cl_matrix(self, fname_save=None, recompute=False):
         """ Computes matrix of power spectra between all the initial groups.
-
         Args:
-            fname_save (string): if not None, the result will be saved to a
+                        fname_save (string): if not None, the result will be saved to a
                 file by this name, and if present and `recompute=False`, the
                 matrix will be read from that file.
             recompute (bool): if `True`, any previous calculations of the
@@ -72,20 +70,23 @@ class SnCalc(object):
                 return
 
         # Cosmology
-        cosmo = ccl.CosmologyVanillaLCDM()
+        cosmo = ccl.Cosmology(Omega_c=0.25,
+                Omega_b=0.05,
+                h=0.67, n_s=0.96,
+                sigma8=0.81)
 
         # Tracers
         if self.use_clustering:
             trs = [ccl.NumberCountsTracer(cosmo, False, (self.z_arr, nz),
-                                          bias=(self.z_arr,
-                                                self._bz_model(cosmo,
-                                                               self.z_arr)))
-                   for nz in self.nz_list]
+                bias=(self.z_arr,
+                    self._bz_model(cosmo,
+                        self.z_arr)))
+                    for nz in self.nz_list]
         else:
             trs = [ccl.WeakLensingTracer(cosmo, (self.z_arr, nz))
-                   for nz in self.nz_list]
+                    for nz in self.nz_list]
 
-        # Cls
+            # Cls
         self.cls = np.zeros([self.n_ell, self.n_samples, self.n_samples])
         for i in range(self.n_samples):
             for j in range(i, self.n_samples):
@@ -100,9 +101,8 @@ class SnCalc(object):
     def get_resample_metadata(self, assign):
         """ Transform an assignment list into a weight matrix and the
         corresponding number densities.
-
         Args:
-            assign (list): list of arrays. Each element of the list should
+                        assign (list): list of arrays. Each element of the list should
                 be a numpy array of integers, corresponding to the indices
                 of the initial groups that are now resampled into a larger
                 group.
@@ -136,9 +136,8 @@ class SnCalc(object):
     def get_sn_wn(self, weights, n_dens, full_output=False):
         """ Compute signal-to-noise ratio from weights matrix and number
         densities of the resampled groups.
-
         Args:
-            weights (array_like): weights matrix of shape
+                        weights (array_like): weights matrix of shape
                 `(N_resample, N_initial)`, where `N_resample` is the
                 number of new groups, and `N_initial` is the original
                 number of groups. Each entry corresponds to the weight
@@ -148,9 +147,8 @@ class SnCalc(object):
                 new groups.
             full_output (bool): if true, a dictionary with additional
                 information will be returned. Otherwise just total S/N.
-
         Returns:
-            If `full_output=True`, dictionary containing S/N, power
+                        If `full_output=True`, dictionary containing S/N, power
             spectra and noise spectra. Otherwise just S/N.
         """
         sl = self._get_cl_resamples(weights)
@@ -159,10 +157,10 @@ class SnCalc(object):
         icl = np.linalg.inv(cl)
         sn2_1pt = np.sum(sl[:, :, :, None] * icl[:, None, :, :], axis=2)
         sn2_ell = np.sum(sn2_1pt[:, :, :, None] * sn2_1pt[:, None, :, :],
-                         axis=2)
+                axis=2)
         trsn2_ell = np.trace(sn2_ell, axis1=1, axis2=2)
         snr = np.sqrt(np.sum(trsn2_ell * (2*self.larr + 1) *
-                             self.d_ell / self.fsky))
+            self.d_ell / self.fsky))
 
         if full_output:
             dret = {'snr': snr, 'cl': sl, 'nl': nl, 'ls': self.larr}
@@ -172,40 +170,36 @@ class SnCalc(object):
 
     def get_sn(self, assign, full_output=False):
         """ Compute signal-to-noise ratio from a bin assignment list.
-
         Args:
-            assign (list): list of arrays. Each element of the list should
+                        assign (list): list of arrays. Each element of the list should
                 be a numpy array of integers, corresponding to the indices
                 of the initial groups that are now resampled into a larger
                 group.
             full_output (bool): if true, a dictionary with additional
                 information will be returned. Otherwise just total S/N.
-
         Returns:
-            If `full_output=True`, dictionary containing S/N, power
+                        If `full_output=True`, dictionary containing S/N, power
             spectra and noise spectra. Otherwise just S/N.
         """
         w, ndens = self.get_resample_metadata(assign)
         return self.get_sn_wn(w, ndens,
-                              full_output=full_output)
+                full_output=full_output)
 
     def check_edges(self, edges):
         """ Returns `True` if there's something wrong with the edges.
         """
         return np.any(edges < 0) or \
-            np.any(edges > self.edges_large) or \
-            np.any(np.diff(edges) < 0)
+                np.any(edges > self.edges_large) or \
+                np.any(np.diff(edges) < 0)
 
     def assign_from_edges(self, edges, assign_params=assign_params_default):
         """ Get assignment list from edges and assign parameters.
-
         Args:
-            edges (array_like): array of bin edges.
+                        edges (array_like): array of bin edges.
             assign_params (dict): dictionary of assignment parameters
                 (see `assign_params_default`).
-
         Returns:
-            List of assignment arrays ready to be used in e.g. `get_sn`.
+                        List of assignment arrays ready to be used in e.g. `get_sn`.
         """
         nbins = len(edges) + 1
         # Bin IDs based on mean z
@@ -217,40 +211,38 @@ class SnCalc(object):
             if assign_params['use_p_outbin']:
                 # Matrix of probabilities in each bin
                 pzs = np.array([[simps(pz[m], x=self.z_arr[m])
-                                 for m in masks]
-                                for pz in self.pz_list])
+                    for m in masks]
+                    for pz in self.pz_list])
                 pzd = np.array([pzs[j, ids[j]]
-                                for j in range(self.n_samples)])
+                    for j in range(self.n_samples)])
             else:
                 # Overlaps in own bin
                 pzd = np.array([simps(pz[masks[i]],
-                                      x=self.z_arr[masks[i]])
-                                for pz, i in zip(self.pz_list, ids)])
+                    x=self.z_arr[masks[i]])
+                    for pz, i in zip(self.pz_list, ids)])
 
-            # Throw away based on in-bin probability
+                # Throw away based on in-bin probability
             if assign_params['use_p_inbin']:
                 ids[pzd < assign_params['p_inbin_thr']] = -1
             # Throw away based on off-bin probability
             if assign_params['use_p_outbin']:
                 ids[np.array([np.sum(p > assign_params['p_outbin_thr']) > 1
-                              for p in pzs])] = -1
-        # Assignment list
+                    for p in pzs])] = -1
+                # Assignment list
         return [np.where(ids == i)[0] for i in np.unique(ids)]
 
     def get_sn_from_edges(self, edges, full_output=False,
-                          assign_params=assign_params_default):
+            assign_params=assign_params_default):
         """ Compute signal-to-noise ratio from a set of edges and assignment
         parameters.
-
         Args:
-            edges (array_like): array of bin edges.
+                        edges (array_like): array of bin edges.
             assign_params (dict): dictionary of assignment parameters
                 (see `assign_params_default`).
             full_output (bool): if true, a dictionary with additional
                 information will be returned. Otherwise just total S/N.
-
         Returns:
-            If `full_output=True`, dictionary containing S/N, power
+                        If `full_output=True`, dictionary containing S/N, power
             spectra and noise spectra. Otherwise just S/N.
         """
         if self.check_edges(edges):
