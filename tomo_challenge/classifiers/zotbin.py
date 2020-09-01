15e85b4af66e834c7d1587e16f90ffa73e38bcb4
David Kirkby <dkirkby@uci.edu>
2020-08-31 20:26:33 -0700

    Flesh out tomo class

--- tomo_challenge/classifiers/zotbin.py
+++ tomo_challenge/classifiers/zotbin.py
@@ -1,9 +1,16 @@
 from .base import Tomographer
 import numpy as np
 
+try:
+    from zotbin.group import groupbins
+    from zotbin.binned import get_zedges_chi
+except ImportError:
+    print('You need to install the zotbin package:\n  pip install git+https://github.com/dkirkby/zotbin.git')
 
-class RandomForest(Tomographer):
-    """ Random Forest Classifier """
+
+class ZotBin(Tomographer):
+    """ ZotBin method.
+    """
 
     # valid parameter -- see below
     valid_options = ['bins']
@@ -30,8 +37,16 @@ class RandomForest(Tomographer):
         """
         self.bands = bands
         self.opt = options
+        if self.opt['loadgroups'] is not None:
+            self.zedges, self.fedges, self.grpid, _, _ = load_groups(self.opt['loadgroups'])
 
-    def train (self, training_data, training_z):
+    def prepare(self, data, band='i'):
+        # Use colors and i-band magnitude as the training features.
+        colors = np.diff(data, axis=1)
+        i = self.bands.index(band)
+        return np.concatenate((colors, data[:, i:i + 1]), axis=1)
+
+    def train (self, data, z):
         """Trains the classifier
 
         Parameters:
@@ -43,10 +58,24 @@ class RandomForest(Tomographer):
           true redshift for the training sample
 
         """
-        pass
+        npct = self.opt['npct']
+        nzbin = self.opt['nzbin']
+        ngrp = self.opt['ngrp']
+        weighted = self.opt['weighted']
+        # Prepare the training data.
+        X = self.prepare(data)
+        # Calculate redshift slices that are equally spaced in comoving distance.
+        self.zedges = get_zedges_chi(z, nzbin)
+        # Calculate feature-space groups.
+        self.fedges, self.grpid, zhist, zsim = groupbins(
+            X, z, self.zedges, npct, min_groups=ngrp, weighted=weighted)
+        if self.opt['savegroups'] is not None:
+            save_groups(self.opt['groupfile'], self.zedges, self.fedges, self.grpid, zhist, zsim)
 
     def apply (self, data):
         """Applies training to the data.
+
+        Note that a bin number of -1 indicates that a galaxy should not be used.
 
         Parameters:
         -----------
@@ -59,4 +88,12 @@ class RandomForest(Tomographer):
           tomographic selection for galaxies return as bin number for
           each galaxy.
         """
-        pass
+        ngrp = self.opt['ngrp']
+        X = self.prepare(data)
+        sample_bin = fdigitize(X, self.fedges)
+        tomo_sel = np.full(sample_bin, -1)
+        for igrp in range(ngrp):
+            grp_bins = np.where(self.grpid == igrp)[0]
+            grp_samples = np.isin(sample_bin, grp_bins)
+            tomo_sel[grp_samples] = igrp
+        return tomo_sel
