0cc731a6a071cde2a3f8565be2af94470c34d525
Clecio De Bom <29743719+cdebom@users.noreply.github.com>
2020-09-15 02:27:40 -0300

    Update de_resnet.py

--- tomo_challenge/classifiers/de_resnet.py
+++ tomo_challenge/classifiers/de_resnet.py
@@ -142,5 +142,5 @@ class Classifier_RESNET:
 		return  y_pred
 
 def get_available_gpus():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+	local_device_protos = device_lib.list_local_devices()
+	return [x.name for x in local_device_protos if x.device_type == 'GPU']
