be6ad1b56803655eb405fcd30f5f19615c64e08a
Erfan Nourbakhsh <erfanxyz@gmail.com>
2020-09-22 10:14:29 -0700

    Move everything to the Class

--- tomo_challenge/data.py
+++ tomo_challenge/data.py
@@ -77,7 +77,7 @@ def download_data():
             urlretrieve(f'{url_root_buzzard}/{filename}', f'data_buzzard/{filename}', reporthook=progress)
             
 
-def load_mags(filename, bands, errors=False, band_triplets_errors=False, heal_undetected=False):
+def load_mags(filename, bands, errors=False):
 
 
     # Warn about non-detections being set mag=30.
@@ -86,10 +86,7 @@ def load_mags(filename, bands, errors=False, band_triplets_errors=False, heal_un
     # Best guess is one of the libraries we load sets some option.
     global warned
     if not warned:
-        if heal_undetected:
-            warnings.warn("Setting inf (undetected) bands to the mag for which S/N=1 (i.e. mag_err=2.5*log10(2))")
-        else:
-            warnings.warn("Setting inf (undetected) bands to mag=30 and mag_err=30")
+        warnings.warn("Setting inf (undetected) bands to mag=30")
         warned = True
 
     data = {}
@@ -99,30 +96,21 @@ def load_mags(filename, bands, errors=False, band_triplets_errors=False, heal_un
         for b in bands:
             data[b] = f[f'{b}_mag'][:]
 
-            if errors or band_triplets_errors:
+            if errors:
                 data[f'{b}_err'] = f[f'{b}_mag_err'][:]
 
-    # Set undetected objects to S/N=1 mags and errors if heal_undetected=True, otherwise to mag 30 +/- 30
-    print(f'Analyzing sample from {filename}')
+
+    # Set undetected objects to mag 30 +/- 30
     for b in bands:
         bad = ~np.isfinite(data[b])
-        if heal_undetected:
-            mag_err_snr1=2.5*np.log10(2)
-            mag_snr1=np.interp(mag_err_snr1, data[f'{b}_err'][~bad], data[b][~bad])
-            data[b][bad] = mag_snr1
-            print(f'band {b}: S/N~1, mag={mag_snr1}, mag_err={mag_err_snr1}')
-        else:
-            data[b][bad] = 30.0
+        data[b][bad] = 30.0
 
-        if errors or band_triplets_errors:
-            if heal_undetected:
-                data[f'{b}_err'][bad] = mag_err_snr1
-            else:
-                data[f'{b}_err'][bad] = 30.0
+        if errors:
+            data[f'{b}_err'][bad] = 30.0
 
     return data
 
-def add_colors(data, bands, errors=False, band_triplets_errors=False):
+def add_colors(data, bands, errors=False):
     nband = len(bands)
     nobj = data[bands[0]].size
     ncolor = nband * (nband - 1) // 2
@@ -133,10 +121,11 @@ def add_colors(data, bands, errors=False, band_triplets_errors=False):
     # on the choice to set undetected magnitudes to 30.
     for b,c in colors_for_bands(bands):
         data[f'{b}{c}'] = data[f'{b}'] - data[f'{c}']
-        if errors or band_triplets_errors:
+        if errors:
             data[f'{b}{c}_err'] = np.sqrt(data[f'{b}_err']**2 + data[f'{c}_err']**2)
 
-def dict_to_array(data, bands, errors=False, colors=False, band_triplets=False, band_triplets_errors=False):
+
+def dict_to_array(data, bands, errors=False, colors=False):
     nobj = data[bands[0]].size
     nband = len(bands)
     ncol = nband
@@ -144,10 +133,6 @@ def dict_to_array(data, bands, errors=False, colors=False, band_triplets=False, 
         ncol += nband * (nband - 1) // 2
     if errors:
         ncol *= 2
-    if band_triplets:
-        ncol += (nband - 2)
-    if band_triplets_errors:
-        ncol += (nband - 2)
 
     arr = np.empty((ncol, nobj))
     i = 0
@@ -159,7 +144,7 @@ def dict_to_array(data, bands, errors=False, colors=False, band_triplets=False, 
         for b, c in colors_for_bands(bands):
             arr[i] = data[f'{b}{c}']
             i += 1
-            
+
     if errors:
         for b in bands:
             arr[i] = data[f"{b}_err"]
@@ -169,17 +154,7 @@ def dict_to_array(data, bands, errors=False, colors=False, band_triplets=False, 
         for b, c in colors_for_bands(bands):
             arr[i] = data[f'{b}{c}_err']
             i += 1
-    
-    if band_triplets:
-        for (b1,b2), (c1,c2) in band_triplets_for_bands(bands):
-            arr[i] = data[f'{b1}{b2}']-data[f'{c1}{c2}']
-            i += 1
 
-    if band_triplets_errors:
-        for (b1,b2), (c1,c2) in band_triplets_for_bands(bands):
-            arr[i] = np.sqrt(data[f'{b1}{b2}_err']**2+data[f'{c1}{c2}_err']**2)
-            i += 1
-            
     return arr.T
 
 
@@ -188,24 +163,16 @@ def colors_for_bands(bands):
         for c in bands[i+1:]:
             yield b, c
 
-def adjacent_colors_for_bands(bands):
-    for i,b in enumerate(bands[0:-1]):
-        yield b, bands[i+1]
-        
-def band_triplets_for_bands(bands):
-    clrs = list(adjacent_colors_for_bands(bands))
-    bts = colors_for_bands(clrs)
-    bts_selected = [bt for bt in bts if bt[0][1]==bt[1][0]] # difference of two adjacent colors, e.g. (g-r)-(r-i)
-    return bts_selected
 
-def load_data(filename, bands, colors=False, errors=False, band_triplets=False, band_triplets_errors=False, array=False, heal_undetected=False):
-    data = load_mags(filename, bands, errors=errors, band_triplets_errors=band_triplets_errors, heal_undetected=heal_undetected)
+
+def load_data(filename, bands, colors=False, errors=False, array=False):
+    data = load_mags(filename, bands, errors=errors)
 
     if colors:
-        add_colors(data, bands, errors=errors, band_triplets_errors=band_triplets_errors)
+        add_colors(data, bands, errors=errors)
 
     if array:
-        data = dict_to_array(data, bands, errors=errors, colors=colors, band_triplets=band_triplets, band_triplets_errors=band_triplets_errors)
+        data = dict_to_array(data, bands, errors=errors, colors=colors)
 
     return data
 
