dcd4a08949ee0da1ca300d53b3a77a3b51342bdf
joezuntz <joezuntz@googlemail.com>
2020-12-02 06:17:32 -0800

    work

--- bin/run_metrics.py
+++ bin/run_metrics.py
@@ -1,18 +1,22 @@
-import tomo_challenge
-
-
+#import tomo_challenge
+import numpy as np
+import glob
 
 # with h5py.File("data/validation.hdf5", "r") as f:#change later
 #     truth_z = f["redshift_true"][:]
 
 
-completed = glob.glob("/global/cscratch1/sd/zuntz/tomo_challenge_results/*.npy")
+results_dir = "/global/cscratch1/sd/zuntz/tomo_challenge_results/"
+completed = sorted(glob.glob(results_dir + "*.npy"))
 
 for result_file in completed:
     bins = np.load(result_file).astype(int)
 
     unique_bins = np.unique(bins)
-    print(result_file, unique_bins)
+    print(result_file[len(results_dir):])
+    for b in unique_bins:
+        print("    ", b, (bins==b).sum())
+
 #     for b in unique_bins:
 
 
