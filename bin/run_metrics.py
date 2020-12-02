67bf4da02d1cf8f164eabc79e25d6431e81e6c88
Joe Zuntz <joezuntz@googlemail.com>
2020-12-02 14:22:30 +0000

    print metrics

--- bin/run_metrics.py
+++ bin/run_metrics.py
@@ -1,9 +1,9 @@
-#import tomo_challenge
+import tomo_challenge
 import numpy as np
 import glob
 
-# with h5py.File("data/validation.hdf5", "r") as f:#change later
-#     truth_z = f["redshift_true"][:]
+with h5py.File("data/validation.hdf5", "r") as f:#change later
+    truth_z = f["redshift_true"][:]
 
 
 results_dir = "/global/cscratch1/sd/zuntz/tomo_challenge_results/"
@@ -14,8 +14,25 @@ for result_file in completed:
 
     unique_bins = np.unique(bins)
     print(result_file[len(results_dir):])
+
+    if -1 in unique_bins:
+        # if this is just a tiny number then put them in bin 0
+        if (bins==-1).sum() < 0.03 * bins.size:
+            bins[bins==-1] = 0
+        # otherwise push everything up by one
+        else:
+            bins += 1
+            unique_bins += 1
+
     for b in unique_bins:
         print("    ", b, (bins==b).sum())
+
+    metrics = tomo_challenge.jc_compute_scores(bins, truth_z)
+    for name, value in metrics:
+        print("    ", name, value)
+
+
+
 
 #     for b in unique_bins:
 
